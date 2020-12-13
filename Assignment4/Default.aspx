04a3e23c35e528272e930134c230bca114cf8b0f
sagarmanku03 <71673218+sagarmanku03@users.noreply.github.com>
2020-12-13 21:40:10 +0530

    Initial commit

---
+++ Assignment4/Default.aspx
@@ -0,0 +1,125 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment4.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" 
+             ShowHeaderWhenEmpty="true"
+             OnRowCommand="GridView1_RowCommand" OnRowEditing="GridView1_RowEditing" OnRowCancelingEdit="GridView1_RowCancelingEdit"
+              OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting"
+            ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" AutoGenerateColumns="false" ShowFooter="true" DataKeyNames="Id"> 
+           
+             <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+
+            <Columns>
+                <asp:TemplateField HeaderText="Booking ID">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Id") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtId" Text='<%# Eval("Id") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtIdFooter"  runat="server" />
+                    </FooterTemplate>
+                 </asp:TemplateField>
+           
+                <asp:TemplateField HeaderText="Booking CustomerName">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("CustomerName") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtCustomerName" Text='<%# Eval("CustomerName") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtCustomerNameFooter"  runat="server" />
+                    </FooterTemplate>
+                 </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Booking MovieName">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("MovieName") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtMovieName" Text='<%# Eval("MovieName") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtMovieNameFooter"  runat="server" />
+                    </FooterTemplate>
+                 </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Booking PhoneNo">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("PhoneNo") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtPhoneNo" Text='<%# Eval("PhoneNo") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtPhoneNoFooter"  runat="server" />
+                    </FooterTemplate>
+                 </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Booking Address">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Address") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtAddress" Text='<%# Eval("Address") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtAddressFooter"  runat="server" />
+                    </FooterTemplate>
+                 </asp:TemplateField>
+
+               <asp:TemplateField>
+
+                   <ItemTemplate>
+
+                       <asp:ImageButton Imageurl="~/imz/edit.png" runat="server" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px" />
+                        <asp:ImageButton Imageurl="~/imz/delete.png" runat="server" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px" />
+
+                   </ItemTemplate>
+
+                   <EditItemTemplate>
+                       <asp:ImageButton Imageurl="~/imz/save.png" runat="server" CommandName="Save" ToolTip="Save" Width="20px" Height="20px" />
+                        <asp:ImageButton Imageurl="~/imz/cancel.png" runat="server" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px" />
+                   </EditItemTemplate>
+                   
+                   <FooterTemplate>
+                       <asp:ImageButton Imageurl="~/imz/add.png" runat="server" CommandName="Add" ToolTip="Add" Width="20px" Height="20px" />
+                   </FooterTemplate>
+
+               </asp:TemplateField>
+           </Columns>
+
+
+        </asp:GridView>
+
+        <br />
+        <asp:Label ID="Lblsuccessmessage" Text="" runat="server" ForeColor="Green" />
+
+                <br />
+        <asp:Label ID="Lblerrormessage" Text="" runat="server" ForeColor="Red" />
+
+            </div>
+    </form>
+</body>
+</html>
---
+++ Assignment4/Default.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Assignment4
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\Assignment4.mdf;Integrated Security=True;Connect Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                populategridview();
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void populategridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlcon = new SqlConnection (connectionstring))
+    {
+    sqlcon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("select * from Booking", sqlcon);
+                
+                sqlDa.Fill(dtbl);
+               
+    }
+            if (dtbl.Rows.Count > 0)
+            {
+                GridView1.DataSource = dtbl;
+                GridView1.DataBind();
+            }
+            else
+            { 
+                dtbl.Rows.Add(dtbl.NewRow());
+            
+                GridView1.DataSource = dtbl;
+                GridView1.DataBind();
+                GridView1.Rows[0].Cells.Clear();
+                GridView1.Rows[0].Cells.Add(new TableCell());
+                GridView1.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                GridView1.Rows[0].Cells[0].Text = "No data Found ..!";
+                GridView1.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("Add"))
+                {
+                    using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+                    {
+                        sqlcon.Open();
+                        string query = "INSERT INTO Booking (Id,CustomerName,MovieName,PhoneNo,Address) VAlUES (@id,@name,@mname,@pn,@ad)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlcon);
+                        sqlCmd.Parameters.AddWithValue("@id", (GridView1.FooterRow.FindControl("txtIdFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@name", (GridView1.FooterRow.FindControl("txtCustomerNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@mname", (GridView1.FooterRow.FindControl("txtMovieNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@pn", (GridView1.FooterRow.FindControl("txtPhoneNoFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@ad", (GridView1.FooterRow.FindControl("txtAddressFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        populategridview();
+                        Lblsuccessmessage.Text = "New Recoard Added";
+                        Lblerrormessage.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Lblsuccessmessage.Text = "";
+                Lblerrormessage.Text = ex.Message;
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            populategridview();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            populategridview();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+                {
+                    sqlcon.Open();
+                    string query = "UPDATE Booking SET Id=@id,CustomerName=@name,MovieName=@mname,PhoneNo=@pn,Address=@ad WHERE Id=@id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlcon);
+                    sqlCmd.Parameters.AddWithValue("@id", (GridView1.Rows[e.RowIndex].FindControl("txtId") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@name", (GridView1.Rows[e.RowIndex].FindControl("txtCustomerName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@mname", (GridView1.Rows[e.RowIndex].FindControl("txtMovieName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@pn", (GridView1.Rows[e.RowIndex].FindControl("txtPhoneNo") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@ad", (GridView1.Rows[e.RowIndex].FindControl("txtAddress") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    GridView1.EditIndex = -1;
+                    populategridview();
+                    Lblsuccessmessage.Text = "Recoard Updated";
+                    Lblerrormessage.Text = "";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Lblsuccessmessage.Text = "";
+                Lblerrormessage.Text = ex.Message;
+            }
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+                {
+                    sqlcon.Open();
+                    string query = "DELETE FROM Booking Where Id=@id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlcon);                    
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    populategridview();
+                    Lblsuccessmessage.Text = "Recoard deleted";
+                    Lblerrormessage.Text = "";
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Lblsuccessmessage.Text = "";
+                Lblerrormessage.Text = ex.Message;
+            }
+        }
+    }
+}
